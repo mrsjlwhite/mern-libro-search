51a008c41d06d86347a3cd25ea4fb35ea052c65b
Jeslyn White <jlwhite.codes@gmail.com>
2022-05-14 01:11:20 -0700

    Removing comment

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks', 
   {
-    useNewUrlParser: true,
+    useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true,
-    // useFindAndModify: false,
+    useFindAndModify: false
   }
 );
 
